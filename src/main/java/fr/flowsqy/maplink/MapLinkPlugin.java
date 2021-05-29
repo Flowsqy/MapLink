79327ba7e34d1db3562e4d111b0c48fe5304d4ab
Flowsqy <Flo504.Dev@gmail.com>
2021-05-29 18:06:30 +0200

    Create project structure

---
+++ src/main/java/fr/flowsqy/maplink/MapLinkPlugin.java
@@ -0,0 +1,11 @@
+package fr.flowsqy.maplink;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MapLinkPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
